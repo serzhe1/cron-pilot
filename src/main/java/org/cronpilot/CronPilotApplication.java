d8b56151210278a62b3404dc032674655fd69057
s.ladygin <s.a.ladygin@yandex.ru>
2025-10-04 22:24:23 +0300

    init spring project

---
+++ src/main/java/org/cronpilot/CronPilotApplication.java
@@ -0,0 +1,13 @@
+package org.cronpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronPilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronPilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cronpilot/CronPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cronpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronPilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
